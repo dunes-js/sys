26d2944a048afcc83e1a6b62d45ec817cd46d129
Omar <omarfkuri@gmail.com>
2024-01-30 01:35:30 -0600

    Added safe clean

--- src/dir.ts
+++ src/dir.ts
@@ -1,3 +1,4 @@
+import { existsSync } from "fs";
 import { readdir, rm } from "fs/promises";
 import { join } from "path";
 
@@ -6,5 +7,13 @@ export async function cleanDir(name: string)
   for (const fileName of await readdir(name))
   {
     await rm(join(name, fileName), {recursive: true})
+  }
+}
+
+export async function safeCleanDir(name: string)
+{
+  if (existsSync(name))
+  {
+    await cleanDir(name);
   }
 }
